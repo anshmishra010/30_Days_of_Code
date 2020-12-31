634e1ae4dde540a0e84047ed852a655aa34756fd
Ansh mishra <anshmishra9415@gmail.com>
2020-12-31 13:28:18 +0530

    hackerrank

---
+++ Day 3_Intro to Conditional Statements.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+if (n % 2 != 0):
+    print("Weird")
+
+# If  is even and in the inclusive range of  to , print Not Weird
+elif (n % 2 == 0 and 2 <= n <= 5):
+    print("Not Weird")
+
+    # if(6<=n<=20):
+    #     print("Weird")
+    # elif(n>20):
+    #     print("Not Weird")
+elif (n % 2 == 0 and 6 <= n <= 20):
+    print("Weird")
+elif (n % 2 == 0 and n > 20):
+    print("Not Weird")
