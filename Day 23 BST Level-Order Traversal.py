115c95fabbea4cdb7cbd2145b9fe41f08be78cf0
Ansh mishra <anshmishra9415@gmail.com>
2021-01-22 02:09:42 +0530

    hackerrank

---
+++ Day 23 BST Level-Order Traversal.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self,data):
+        self.right=None
+        self.left=None
+        self.data=data
+class Solution:
+    def insert(self,root,data):
+        if root == None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur =self.insert(root.right,data)
+                root.right=cur
+        return root
+    def levelOrder(self,root):
+        if root is None:
+            return
+        queue=[]
+        queue.append(root)
+        while(len(queue)>0):
+            node=queue.pop(0)
+            print(node.data,end=' ')
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+        return
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data = int(input())
+    root = myTree.insert(root,data)
+myTree.levelOrder(root)
